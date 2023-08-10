f0680571ea7b28fdf01062b07f1b3f9ba9f6e844
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-08-10 20:08:30 -0300

    validando frontEnd cadastro

--- frontend/modules/cadastro.js
+++ frontend/modules/cadastro.js
@@ -2,63 +2,103 @@ import isEmail from 'validator/lib/isEmail';
 
 const validator = require('validator');
 
-export default class CadastraContato{
-    constructor(formClass){
+export default class CadastraContato {
+    constructor(formClass) {
         this.form = document.querySelector(formClass);
     }
 
-    init(){
+    init() {
         this.events();
     }
 
-    events(){
-        if(!this.form) return;
-        
+    events() {
+        if (!this.form) return;
+
         this.form.addEventListener('submit', e => {
             e.preventDefault();
             this.validate(e);
         });
     }
 
-    validate(e){
+    generatesFieldError(field, text) {
+        field.classList.add('is-invalid');
+
+        const divError = document.createElement('div');
+        divError.classList.add('invalid-feedback');
+
+        divError.innerHTML = `${text}`.toUpperCase();
+
+        field.insertAdjacentElement('afterend', divError);
+    }
+
+    generatesGeneralError(field, text) {
+        const divError = document.createElement('div');
+        //classe d-block adicionada para remover a estilização padrao do boostrap para campos invalidos: display:none
+        divError.classList.add('invalid-feedback', 'd-block', 'text-center');
+
+        divError.innerHTML = `${text}`.toUpperCase();
+
+        field.insertAdjacentElement('afterend', divError);
+        return divError;
+    }
+
+    containsNumber(string) {
+        return /\d/.test(string);
+    }
+
+    validate(e) {
         //pegando o elemento que disparou o evento submit (formulario submetido) e pegando o input específico
         const el = e.target;
         let error = false;
+
 
         const inputName = el.querySelector('input[name="nome"]');
         const inputSurname = el.querySelector('input[name="sobrenome"]');
         const inputEmail = el.querySelector('input[name="email"]');
         const inputPhone = el.querySelector('input[name="telefone"]');
-        
-        if(inputName.value.length > 30 || inputName.value.length <= 1){
-            error = true;
-            const divError = document.querySelector('.div-error');
-            inputName.classList.add('is-invalid');
-            divError.classList.add('invalid-feedback');
-            divError.textContent = 'Nome inválido';
+        const paragraph = document.querySelector('.paragraph');
+        const elementsOfDocument = document.querySelectorAll('*');
+
+
+        for (let msgError of this.form.querySelectorAll('.invalid-feedback')) {
+            msgError.remove();
+        };
+
+        for (let msgError of this.form.querySelectorAll('.is-invalid')) {
+            msgError.classList.remove('is-invalid');
+        };
+
+        for (let msgError of elementsOfDocument) {
+            if (msgError.classList.contains('d-block')) {
+                msgError.remove();
+            }
         }
 
-        if(inputSurname.value.length > 30 || inputName.value.length <= 1){
+        if (inputName.value.length > 30 || inputName.value.length <= 1 || this.containsNumber(inputName.value)) {
             error = true;
-            alert('sobrenome invalido');
+            return this.generatesFieldError(inputName, `${inputName.name} inválido`);
         }
 
-        if(!validator.isEmail(inputEmail.value)){
+        if (inputSurname.value.length > 30 || this.containsNumber(inputSurname.value)) {
             error = true;
-            alert('email invalido');
+            this.generatesFieldError(inputSurname, `${inputSurname.name} inválido`);
         }
 
-        if(!inputPhone.value){
+        // if (!inputPhone.value) {
+        //     error = true;
+        //     inputPhone.value = '';
+        // }
+
+        if (inputPhone.value.length <= 0 && inputEmail.value.length <= 0) {
             error = true;
-            inputPhone.value = '';
+            this.generatesGeneralError(paragraph, `Os campos ${inputPhone.name} ou ${inputEmail.name} devem ser preechidos`)
+        } else if (!validator.isEmail(inputEmail.value) && !validator.isMobilePhone(inputPhone.value, 'pt-BR')) {
+            error = true;
+            this.generatesGeneralError(paragraph, `${inputPhone.name} ou ${inputEmail.name} inválido`)
         }
 
-        if(!validator.isMobilePhone(inputPhone.value, 'pt-BR')){
-            error = true;
-            alert('numero de celular invalido');
-        }
-        
+
         //se não tiver erro executa o submite do form
-        if(!error) el.submit();
+        if (!error) el.submit();
     }
 } 
